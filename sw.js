409af08433354f67431226802b15512cb1b93c1b
Chris <zhaodonglai@Mac-mini.local>
2025-01-24 20:20:13 +0800

    增加对fech的处理

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
 const CACHE_NAME = 'japanese-learning-cache-v1';
 const DICTIONARY_CACHE = 'dictionary-cache-v1';
 
+// 词典文件列表
+const dictionaryFiles = [
+    'base.dat.gz',
+    'cc.dat.gz',
+    'check.dat.gz',
+    'tid.dat.gz',
+    'tid_map.dat.gz',
+    'tid_pos.dat.gz',
+    'unk.dat.gz',
+    'unk_char.dat.gz',
+    'unk_compat.dat.gz',
+    'unk_invoke.dat.gz',
+    'unk_map.dat.gz',
+    'unk_pos.dat.gz'
+];
+
 const urlsToCache = [
     '/',
     '/index.html',
@@ -25,10 +41,38 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
-    // 处理其他资源请求
+
+    // 处理 chrome-extension 请求
     if (event.request.url.startsWith('chrome-extension://')) {
         return;
     }
+
+    // 处理词典文件请求
+    if (url.href.includes('kuromoji.js@master/dict/')) {
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request)
+                        .then(response => {
+                            // 处理 301 跳转
+                            if (response.redirected) {
+                                const responseToCache = response.clone();
+                                caches.open(DICTIONARY_CACHE)
+                                    .then(cache => {
+                                        cache.put(event.request, responseToCache);
+                                    });
+                            }
+                            return response;
+                        });
+                })
+        );
+        return;
+    }
+
+    // 处理其他资源请求
     event.respondWith(
         caches.match(event.request)
             .then(response => {
